72d8f4eba3227f439c8b168322cf17ee175266cb
Apostolos Karamanis <apostkaram@gmail.com>
2025-01-10 00:11:09 +0200

    working on marketplace support

--- src/jsx/ProductGrid.jsx
+++ src/jsx/ProductGrid.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../css/ProductGrid.css';
 import ProductCard from './ProductCard';
-import { marketplaceService } from './api/MarketplaceService.js';
+import { marketplaceService } from '../api/MarketplaceService.js';
 
 const ProductGrid = ({ loading }) => {
     // if (loading) {
